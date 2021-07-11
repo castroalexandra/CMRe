d7eb297b107720b31bcac7f20cf6e8d6e1074d2f
Alexandra Castro <alexandracastro@ipvc.pt>
2021-07-11 18:00:56 +0100

    login automatico

--- app/src/main/java/com/example/cmre/MainActivity.kt
+++ app/src/main/java/com/example/cmre/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.cmre
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -12,8 +14,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun abrirMapas(view: View) {
-        val intent = Intent(this, LoginActivity::class.java)
+        val sharedPref: SharedPreferences = getSharedPreferences("LoginSP", Context.MODE_PRIVATE)
+        var loggedIn = false
+
+        if (sharedPref != null){
+            if(sharedPref.all["estadoLogin"]==true){
+                loggedIn = true
+            }
+        }
+
+
+        var intent : Intent
+        if(loggedIn){
+            intent = Intent(this, MapsActivity::class.java)
+        } else {
+            intent = Intent(this, LoginActivity::class.java)
+        }
         startActivity(intent)
+        finish()
     }
 
     fun abrirNotas(view: View) {
